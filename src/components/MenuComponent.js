93f5e171c46faa5fe5bb889ee58008cdce3deb13
Venkat Ganesh <neelavenkatganesh16@gmail.com>
2020-07-01 15:37:17 +0530

    Lifecycle Methods Part 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,73 +1,73 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React, { Component } from "react";
+import {Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle } from "reactstrap";
 /*creating new component */
 class Menu extends Component {
-/*The purpose of a constructor is to create an object and set values if there are any object properties present.*/
-    constructor(props) {
-        /* supply props to super class */
-        super(props);
-/* Each component can store its local information in its "state" private and fully controlled by the component */
+    /*The purpose of a constructor is to create an object and set values if there are any object properties present.*/
+  constructor(props) {
+      /* supply props to super class */
+    super(props);
+    /* Each component can store its local information in its "state" private and fully controlled by the component */
 /* can be passed as props to children */
-        this.state = {
-/*no longer need the dishes in the state of the menu component, because I am receiving that as props from my parent, which is the App component*/
-/* now the dishes array copied to shared folder so that it can be used in other components also */          
-            selectedDish: null
-        }
-    }
 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
-    }
+    this.state = {
+        /*no longer need the dishes in the state of the menu component, because I am receiving that as props from my parent, which is the App component*/
+/* now the dishes array copied to shared folder so that it can be used in other components also */
+      selectedDish: null,
+    };
+  }
 
-    renderDish(dish) {
-        if (dish != null) {
-            return(
-                <Card>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>    
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>  
-                </Card>
-            );
-        }
-        else {
-            return(
-                <div></div>  
-            );
-        }
-    }
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish });
+  }
+
+  renderDish(dish) {
+    if (dish != null)
+        return(
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    else
+        return(
+            <div></div>
+        );
+}
 /* render() will return the corresponding view for the component*/
-    render() {
-        /* defining a javascript const*/
+  render() {
+      /* defining a javascript const*/
         /*this means that I will be iterating over every dish in the dishes array here.*/
         /* this.state.dishes.map changed to this.props.dishes.map so it can fetch the dishes array from my parent */
-        const menu =  this.props.dishes.map((dish) => {
-            return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick={() => this.onDishSelect(dish)}>
-                    <CardImg width="100%" object src={dish.image} alt={dish.name} />
-                    <CardImgOverlay>
-                        <CardTitle>{dish.name}</CardTitle>
-                    </CardImgOverlay>
-                </Card>
-              </div>
-            );
-        });
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-1 ">
+          <Card onClick={() => this.onDishSelect(dish)} tag="li">
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
 
-        return (
-            /* media class is going to display a list of items*/
-            /* {menu} is a javascript variable which is defined above*/
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
-                </div>
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
             </div>
-        );
-    }
+            <div className="row">
+              <div  className="col-12 col-md-5 m-1">
+                {this.renderDish(this.state.selectedDish)}
+              </div>
+            </div>
+        </div>
+    );
+  }
 }
 
 export default Menu;
