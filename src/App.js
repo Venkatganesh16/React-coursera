25af6498378100c363cc75e9fdb70bab611786aa
Venkat Ganesh <neelavenkatganesh16@gmail.com>
2020-07-04 12:20:03 +0530

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import {Navbar, NavbarBrand} from "reactstrap"; //react implementation of bootstrap
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-/* now my state information that contains all the dishes is now lifted into the App.js file. I can make this available to the menu component through props from the App.js file. */
-    this.state = {
-      dishes: DISHES
-    };
-  }
   render() {
     return (
        <div>
-       <Navbar dark color="primary">
-           <div className="container">
-            <NavbarBrand href="/">
-            Ristorante Con Fusion
-            </NavbarBrand>
-           </div>
-       </Navbar>
-       <Menu dishes={this.state.dishes} />
+         <Main />
        </div>
-       /*make those dishes available as props to the menu component*/
     );
   }
 }
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,62 +1,62 @@
+  
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component{
+class DishDetail extends Component {
 
-    constructor(props) {
-        super(props);
-    }
-
-
-    renderDish(dish){
-        if(dish!=null){
-             return( <Card>
-                <CardImg width='100%' src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
+    renderDish(dish) {
+        return (
+            <Card>
+                <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
                 </CardBody>
-           </Card>);
-        } else {
-            return <div></div>;
-        }
+            </Card>
+        );
     }
 
-    renderComments(comments){
-        const getComments = comments.map((comment) => {
+    renderComments(comments) {
+        var commentList = comments.map(comment => {
             return (
-                <div key={comment.id} >
-                    <ul className="list-unstyled">
+                <ul className="list-unstyled">
                         <li>{comment.comment}</li>
-                        <li>--{comment.author}, {comment.date}.</li>
+                        <li>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+                        <br></br>
                     </ul>
-                </div>
-            )
-        })
-    
+            );
+        });
+
         return (
             <div>
-                <h4 className="h4">Comments</h4>
-                {getComments}
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {commentList}
+                </ul>
             </div>
-        )
+        );
     }
 
-    render(){
-
-        return(
-            <div className="container">
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.dish)}
+    render() {
+        if (this.props.dish) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)}
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderComments(this.props.dish.comments)}
+                        </div>
+                    </div>
                 </div>
-                <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
-                </div>
-            </div>
-        </div>
-        );
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
     }
 }
 
